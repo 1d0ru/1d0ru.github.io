8ab745e2304a1983c924986a9414d095def32018
1d0ru <155214824+1d0ru@users.noreply.github.com>
2024-06-10 11:28:00 -0400

    add lazy loading

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const perPage = 5; // Number of items per page
+const perPage = 5; // Number of pics per load
 let currentPage = 1; // Current page
 let isLoading = false; // Flag to prevent multiple simultaneous requests
 
-// Function to display images for a specific page
+// Function to display images
 function showImages(page) {
     isLoading = true;
     const startIndex = (page - 1) * perPage;
@@ -15,6 +15,7 @@ function showImages(page) {
         const img = document.createElement('img');
         img.src = images[i];
         img.alt = `Photo ${i + 1}`;
+        img.loading = 'lazy';
         photoDiv.appendChild(img);
         gallery.appendChild(photoDiv);
     }
@@ -23,12 +24,12 @@ function showImages(page) {
 }
 
 
-// Function to check if user has scrolled to the bottom of the page
+// check if user has scrolled to the bottom of the page
 function isBottomOfPage() {
     return (window.innerHeight + window.scrollY) >= document.body.offsetHeight;
 }
 
-// Function to handle scroll event
+
 function handleScroll() {
     if (isBottomOfPage() && !isLoading && currentPage * perPage < images.length) {
         currentPage++;
